d4a781d9ef073244bc31540211fc560b6db7a37c
José Alejandro Cortés Taborda <josealejandro221@gmail.com>
2020-06-26 18:20:30 -0500

    Changes on Nav Component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +9,46 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  mobileWidth = 600;
+
+  @HostListener('window:scroll', ['$event'])
+  addMenuBackground = () => {
+    const pageWidth = window.innerHeight;
+    const bodyOffset = document.body.scrollTop || document.documentElement.scrollTop;
+    const navigation = document.querySelector('header nav');
+    if (pageWidth > this.mobileWidth) {
+      bodyOffset > 0 ? navigation.classList.add('py-nav-fixed') : navigation.classList.remove('py-nav-fixed');
+    }
+  }
+
+ /*  onNavItemClick = () => {
+    const navItemList = document.querySelectorAll('.py-section-link');
+    const navItems = Array.from(navItemList);
+
+    navItems.forEach(item => {
+      item.addEventListener("click", event => {
+          event.preventDefault();
+        const sectionId = event.target.getAttribute("href");
+      });
+    })
+  } */
+
+  /* scrollToSection = () => {
+
+  } */
+
   ngOnInit(): void {
   }
+
+  /* addMenuBackground = () => {
+    const bodyOffset= document.body.scrollTop || document.documentElement.scrollTop;
+    const navigation = document.querySelector('header nav');
+
+    bodyOffset > 0 ? navigation.classList.add('py-nav-fixed') : navigation.classList.remove('py-nav-fixed');
+  }
+    window.addEventListener('scroll', () => {
+      this.addMenuBackground();
+    }); */
+
 
 }
